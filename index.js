fdab5944a2e3f35581c65d449e06957500c096cf
Amir9eng <amirmukhtarme@gmail.com>
2020-10-14 17:25:39 +0100

    feat: get one person

--- index.js
+++ index.js
@@ -7,70 +7,72 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-  { 
-    "name": "Arto Hellas", 
+  {
+    "name": "Arto Hellas",
     "number": "040-123456",
     "id": 1
   },
-  { 
-    "name": "Ada Lovelace", 
+  {
+    "name": "Ada Lovelace",
     "number": "39-44-5323523",
     "id": 2
   },
-  { 
-    "name": "Dan Abramov", 
+  {
+    "name": "Dan Abramov",
     "number": "12-43-234345",
     "id": 3
   },
-  { 
-    "name": "Mary Poppendieck", 
+  {
+    "name": "Mary Poppendieck",
     "number": "39-23-6423122",
     "id": 4
   }
-  ]
-app.get('/api/notes/:id' , (req , res ) => {
-  const {id} = req.params
-
-  const note = notes.find(note => note.id === parseInt(id))
-
-  if(!note) return res.status(404).send(`Note with id:${id} was not found`)
-
-  res.json(note)
-
+]
+app.get('/api/persons/:id', (req, res) => {
+  const { id } = req.params
+  const person = persons.find(person => person.id === parseInt(id))
+  if (!person) return res.status(404).send(`person with id:${id} was not found`)
+  res.json(person)
 })
 
-app.get('/api/persons/' , (req , res ) => res.json(persons ) )
-
-app.post('/api/persons/' , (req, res) =>{
-
-const newNote = req.body
-notes = notes.concat(newNote)
-res.json(notes)
+app.get('/info/', (req, res) => {
+  let numberOfPerson = persons.length
+  res.send(`Phonebook has info for ${numberOfPerson} people <br /> ${new Date()}`)
 })
 
-app.patch('/api/persons/:id' , (req , res ) => {
-const {id} = req.params
-const noteUpdate = req.body
+app.get('/api/persons/', (req, res) => res.json(persons))
 
-const note = notes.find(note => note.id === parseInt(id))
-notes = notes.map(note => {
-  if(note.id === parseInt(id)){
-    note = {
-      ...note,
-      ...noteUpdate
+app.post('/api/persons/', (req, res) => {
+
+  const newNote = req.body
+  persons= persons.concat(newNote)
+  res.json(persons)
+})
+
+app.patch('/api/persons/:id', (req, res) => {
+  const { id } = req.params
+  const noteUpdate = req.body
+
+  const note = persons.find(note => note.id === parseInt(id))
+  notes = persons.map(note => {
+    if (note.id === parseInt(id)) {
+      note = {
+        ...persons,
+        ...noteUpdate
+      }
     }
-  }
-  return note
+    return note
+  })
+
+  res.status(200).json({
+    ...note,
+    ...noteUpdate
+  })
+
 })
 
-res.status(200).json({
-  ...note,
-  ...noteUpdate
-})
-
-})
-
-const PORT =3001
+const PORT = 3001
 app.listen(PORT)
 console.log('App is running on port', PORT)
+
 
