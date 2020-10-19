1f5861a383088d487fee449a79d3056b047e9403
Amir9eng <amirmukhtarme@gmail.com>
2020-10-19 18:02:40 +0100

    feat: content entries

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // express server
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms: req[content]'))
 app.disable('x-powered-by')
 app.use(cors())
 
